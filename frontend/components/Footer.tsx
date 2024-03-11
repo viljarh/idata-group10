b3fdd3708f99cedaa2eba3c5b2496054777abe0c
Viljar Hoem-Olsen <viljarh@stud.ntnu.no>
2024-03-11 12:35:11 +0100

    git ignore

--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div>
+    <div className="p-4">
       This website is a result of a university group project, performed in the
       course IDATA2301 Web technologies, at NTNU. All the information provided
       here is a result of imagination. Any resemblance with real companies or
