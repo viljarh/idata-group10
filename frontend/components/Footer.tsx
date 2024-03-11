6c5f02cbd3d7c4e0c76a33c166e31849d7ad240f
viljarh <viljar.hoem.olsen@gmail.com>
2024-03-11 01:29:00 +0100

    Added some things on frontend

---
+++ frontend/components/Footer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      This website is a result of a university group project, performed in the
+      course IDATA2301 Web technologies, at NTNU. All the information provided
+      here is a result of imagination. Any resemblance with real companies or
+      products is a coincidence.
+    </div>
+  );
+};
+
+export default Footer;
