a92fd31f10c6fb33e5f5161f125aa094f16d3a60
thomasaakre <thomas_aa@outlook.com>
2024-04-08 17:06:31 +0200

    apitags vehicle entity
    added apitags to vehicle entity

--- backend/src/vehicles/entities/vehicle.entity.ts
+++ backend/src/vehicles/entities/vehicle.entity.ts
@@ -1,1 +1,45 @@
-export class Vehicle {}
+import { Vehicle } from "@prisma/client";
+import { ApiProperty } from "@nestjs/swagger";
+
+export class VehicleEntity{
+    @ApiProperty()
+    vehicleId: number;
+
+    @ApiProperty()
+    manufacture: string;
+
+    @ApiProperty()
+    modelType: string;
+
+
+    @ApiProperty()
+    modelYear: string;
+
+    @ApiProperty()
+    vehicleCategory: string;
+
+    @ApiProperty()
+    transmissionType: string;
+
+    @ApiProperty()
+    fuelType: string;
+
+    @ApiProperty()
+    passengerCapacity: number;
+
+    @ApiProperty()
+    luggageCapacity: number;
+
+    @ApiProperty()
+    extraFeatures: string;
+
+    @ApiProperty()
+    mileage: number;
+
+    @ApiProperty()
+    image: string;
+
+    @ApiProperty()
+    dailyPrice: number;
+
+}
--- backend/src/vehicles/vehicles.controller.ts
+++ backend/src/vehicles/vehicles.controller.ts
@@ -11,21 +11,30 @@ import { VehiclesService } from './vehicles.service';
 import { CreateVehicleDto } from './dto/create-vehicle.dto';
 import { UpdateVehicleDto } from './dto/update-vehicle.dto';
 
+import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { ApiCreatedResponse } from '@nestjs/swagger';
+import { VehicleEntity } from './entities/vehicle.entity';
+
+
 @Controller('vehicles')
+@ApiTags('vehicles')
 export class VehiclesController {
   constructor(private readonly vehiclesService: VehiclesService) {}
 
   @Post()
+  @ApiCreatedResponse({ type: VehicleEntity })
   create(@Body() createVehicleDto: CreateVehicleDto) {
     return this.vehiclesService.create(createVehicleDto);
   }
 
   @Get()
+  @ApiOkResponse ({ type: [VehicleEntity], isArray: true })
   findAll() {
     return this.vehiclesService.findAll();
   }
 
   @Get(':id')
+  @ApiOkResponse ({ type: [VehicleEntity], isArray: true })
   findOne(@Param('id') vehicleId: string) {
     return this.vehiclesService.findOne(+vehicleId);
   }
