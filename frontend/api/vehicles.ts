f4e0e16f95d428fabc3c4bcab9f44c3235e5b27d
Viljar Hoem-Olsen <111789095+viljarh@users.noreply.github.com>
2024-04-16 14:35:24 +0200

    api

---
+++ frontend/api/vehicles.ts
@@ -0,0 +1,15 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    const response = await fetch("http://localhost:3000/vehicles");
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+}
