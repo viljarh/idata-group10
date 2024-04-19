30f00a20ca787d708af8333a50743f91660af67d
Viljar Hoem-Olsen <111789095+viljarh@users.noreply.github.com>
2024-04-19 14:12:36 +0200

    trying to fetch from api

--- frontend/api/users.ts
+++ frontend/api/users.ts
@@ -1,12 +1,24 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+import { useEffect, useState } from "react";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  try {
-    const response = await fetch('http://localhost:3000/users');
-    const data = await response.json();
-    res.status(200).json(data);
-  } catch (error) {
-    console.error('Error fetching data:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+export function Vehicles() {
+  const [user, setUser] = useState(null)
+  const [isLoading, setLoading] = useState(true)
+
+
+  useEffect(() => {
+    fetch('localhost:8080/api/users')
+      .then((res) => res.json())
+      .then((user) => {
+        setUser(user)
+        setLoading(false)
+      })
+  }, [])
+
+  if (isLoading) {
+    console.log('Loading...')
+  }
+
+  if (!user) {
+    console.log('No user data')
   }
 }
--- frontend/api/vehicles.ts
+++ frontend/api/vehicles.ts
@@ -1,17 +1,24 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { useEffect, useState } from "react";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  try {
-    console.log('Fetching vehicles')
-    const response = await fetch("http://localhost:8080/vehicles");
-    console.log(response)
-    const data = await response.json();
-    res.status(200).json(data);
-  } catch (error) {
-    console.error("Error fetching data:", error);
-    res.status(500).json({ error: "Internal Server Error" });
+export function Vehicles() {
+  const [vehicle, setVehicle] = useState(null)
+  const [isLoading, setLoading] = useState(true)
+
+
+  useEffect(() => {
+    fetch('localhost:8080/api/vehicles')
+      .then((res) => res.json())
+      .then((vehicle) => {
+        setVehicle(vehicle)
+        setLoading(false)
+      })
+  }, [])
+
+  if (isLoading) {
+    console.log('Loading...')
+  }
+
+  if (!vehicle) {
+    console.log('No vehicle data')
   }
 }
