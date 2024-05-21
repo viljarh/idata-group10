cecbccb2bc78bee6d15ed504646c34221399e4c1
Viljar Hoem-Olsen <111789095+viljarh@users.noreply.github.com>
2024-05-21 12:54:57 +0200

    added the frontend domain as origin in backend, trying to get connection with backend again

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,7 @@ import { PrimsaClientExceptionFilter } from './prisma-client-excpetion/prisma-cl
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'https://rentalroulette.com'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
--- backend/src/vehicles/dto/create-vehicle.dto.ts
+++ backend/src/vehicles/dto/create-vehicle.dto.ts
@@ -57,7 +57,6 @@ export class CreateVehicleDto {
   @ApiProperty()
   dailyPrice: number;
 
-  @IsNotEmpty()
   @IsString()
   @ApiProperty()
   rentalCompany: string;
