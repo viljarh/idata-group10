cbc5b8e10025cd20e3680606d68a7d1474246e99
Viljar Hoem-Olsen <111789095+viljarh@users.noreply.github.com>
2024-05-22 11:59:28 +0200

    error when deploying, changed ' with &apos;

--- frontend/app/about/page.tsx
+++ frontend/app/about/page.tsx
@@ -12,15 +12,15 @@ export default function AboutPage() {
                     <p className="text-lg text-center">
                          Welcome to Rental Roulette, where your journey begins
                          with a spin of the wheel and ends with the perfect ride
-                         tailored just for you. At Rental Roulette, we've
+                         tailored just for you. At Rental Roulette, we&apos;ve
                          revolutionized the car rental experience by aggregating
                          prices from a myriad of trusted third-party car rental
                          companies. No more hopping from site to site in search
                          of the best deal – our platform puts the power back in
-                         your hands. Whether you're embarking on a spontaneous
+                         your hands. Whether you&apos;re embarking on a spontaneous
                          road trip, planning a family vacation, or simply need a
                          reliable set of wheels for your daily commute, Rental
-                         Roulette ensures that you're always in the driver's
+                         Roulette ensures that you&apos;re always in the driver&apos;s
                          seat when it comes to choice and affordability.
                     </p>
 
@@ -46,7 +46,7 @@ export default function AboutPage() {
                          the vibrant community of this stunning coastal town.
                          Our focus is on providing tailored car rental solutions
                          by aggregating prices exclusively from rental companies
-                         in the Ålesund area. Whether you're exploring the
+                         in the Ålesund area. Whether you&apos;re exploring the
                          fjords, embarking on a scenic coastal drive, or simply
                          navigating the charming streets of Ålesund itself,
                          Rental Roulette ensures that your local car rental
