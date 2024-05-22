ec319afb87c24739aeda5568e92f77a35ac4d5dd
Viljar Hoem-Olsen <111789095+viljarh@users.noreply.github.com>
2024-05-22 23:40:29 +0200

    Added dave and chuck to database

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -4,14 +4,16 @@ import * as bcrypt from 'bcrypt';
 const prisma = new PrismaClient();
 
 async function main() {
-  const hashedPassword = await bcrypt.hash('adminPassword', 10);
+  const adminPassword = await bcrypt.hash('adminPassword', 10);
+  const chuckPassword = await bcrypt.hash('Nunchucks2024', 10);
+  const davePassword = await bcrypt.hash('Dangerous2024', 10);
 
   const adminUser = await prisma.user.upsert({
     where: { emailAddress: 'admin@rentalroulette.com' },
     update: {},
     create: {
       username: 'admin',
-      password: hashedPassword,
+      password: adminPassword,
       firstName: 'Admin',
       lastName: 'User',
       emailAddress: 'admin@rentalroulette.com',
@@ -20,7 +22,35 @@ async function main() {
     },
   });
 
-  console.log({ adminUser });
+  const chuck = await prisma.user.upsert({
+    where: { emailAddress: 'chuck@nunchucks.com' },
+    update: {},
+    create: {
+      username: 'chucknorris',
+      password: chuckPassword,
+      firstName: 'Chuck',
+      lastName: 'Norris',
+      emailAddress: 'chuck@nunchucks.com',
+      phoneNumber: 1234567890,
+      customerType: 'ADMIN',
+    },
+  });
+
+  const dave = await prisma.user.upsert({
+    where: { emailAddress: 'dave@dangerous.com' },
+    update: {},
+    create: {
+      username: 'dave',
+      password: davePassword,
+      firstName: 'Dave',
+      lastName: 'Dangerous',
+      emailAddress: 'dave@dangerous.com',
+      phoneNumber: 1234567890,
+      customerType: 'REGISTRATED',
+    },
+  });
+
+  console.log({ adminUser, chuck, dave });
 }
 
 main()
